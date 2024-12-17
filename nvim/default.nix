85b2e7372d74ee78c5bafa625f0566bb580f176a
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-12-17 20:11:34 -0300

    refactor: auto-import nix files using lib.filesystem
    - Add helper function `allNixFilesNonDefault` to automatically import all .nix files
    - Use lib.filesystem.listFilesRecursive to scan directory recursively
    - Implement smart handling of default.nix files in subdirectories
    - Remove manual imports list in favor of automatic discovery

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,23 +1,38 @@
 {
   inputs,
+  lib,
   system,
   pkgs,
   ...
 }:
+let
+  allNixFilesNonDefault =
+    path:
+    let
+      isDefaultNix = file: builtins.baseNameOf (toString file) == "default.nix";
+      isNixFile = file: lib.hasSuffix ".nix" (toString file);
+      groupByDirectory = files: builtins.groupBy builtins.dirOf (map toString files);
+      filterDefaultNix =
+        filesByDir:
+        builtins.mapAttrs (
+          _dir: files:
+          if builtins.length files == 1 && isDefaultNix (builtins.head files) then
+            files
+          else
+            builtins.filter (file: !isDefaultNix file) files
+        ) filesByDir;
+    in
+    lib.pipe path [
+      lib.filesystem.listFilesRecursive
+      (builtins.filter isNixFile)
+      groupByDirectory
+      filterDefaultNix
+      (filesByDir: lib.flatten (builtins.attrValues filesByDir))
+      (files: map (file: /. + file) files)
+    ];
+in
 {
-  imports = [
-    ./globals.nix
-    ./options.nix
-
-    ./plugins/catppuccin.nix
-    ./plugins/conform.nix
-    ./plugins/lint.nix
-    ./plugins/lz-n.nix
-    ./plugins/mini.nix
-    ./plugins/oil.nix
-    ./plugins/treesitter.nix
-    ./plugins/which-key.nix
-  ];
+  imports = allNixFilesNonDefault ./.;
 
   extraPackages = [ pkgs.ripgrep ];
 
