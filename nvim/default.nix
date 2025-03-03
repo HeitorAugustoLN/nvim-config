dc3ed551a0e0829fd445a9ebb0c4e0a9a5c25c4c
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-03 12:49:14 -0300

    refactor: simplify imports

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,32 +1,13 @@
-{ lib, pkgs, ... }:
-let
-  allNixFilesNonDefault =
-    path:
-    let
-      isDefaultNix = file: builtins.baseNameOf (toString file) == "default.nix";
-      isNixFile = file: lib.hasSuffix ".nix" (toString file);
-      groupByDirectory = files: builtins.groupBy builtins.dirOf (map toString files);
-      filterDefaultNix =
-        filesByDir:
-        builtins.mapAttrs (
-          _dir: files:
-          if builtins.length files == 1 && isDefaultNix (builtins.head files) then
-            files
-          else
-            builtins.filter (file: !isDefaultNix file) files
-        ) filesByDir;
-    in
-    lib.pipe path [
-      lib.filesystem.listFilesRecursive
-      (builtins.filter isNixFile)
-      groupByDirectory
-      filterDefaultNix
-      (filesByDir: lib.flatten (builtins.attrValues filesByDir))
-      (files: map (file: /. + file) files)
-    ];
-in
+{ pkgs, ... }:
 {
-  imports = allNixFilesNonDefault ./.;
+  imports = [
+    ./autocommands.nix
+    ./globals.nix
+    ./options.nix
+    ./performance.nix
+    ./plugins
+  ];
+
   extraPackages = [ pkgs.ripgrep ];
   nixpkgs.config.allowUnfree = true;
 }
---
+++ nvim/plugins/default.nix
@@ -0,0 +1,16 @@
+{
+  imports = [
+    ./blink-cmp.nix
+    ./catppuccin.nix
+    ./conform.nix
+    ./lint.nix
+    ./lsp.nix
+    ./luasnip
+    ./lz-n.nix
+    ./mini.nix
+    ./oil.nix
+    ./todo-comments.nix
+    ./treesitter.nix
+    ./which-key.nix
+  ];
+}
