966094e7c597618f8d2c6a4a9ac1c7b085b6ef6d
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-28 14:15:49 -0300

    feat(mini-surround): set custom mappings
    Set custom mappings for mini-surround to avoid conflicts with the default `s` key in vim/neovim.

--- flake/modules/plugins/mini-surround.nix
+++ flake/modules/plugins/mini-surround.nix
@@ -3,6 +3,15 @@
     plugins.mini-surround = {
       enable = true;
       lazyLoad.settings.event = "DeferredUIEnter";
+
+      settings.mappings = {
+        add = "gsa";
+        delete = "gsd";
+        find = "gsf";
+        find_left = "gsF";
+        highlight = "gsh";
+        replace = "gsr";
+      };
     };
   };
 }
